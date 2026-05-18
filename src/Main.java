8788e1a76f42f41adbb72fa2e94bcf3a5367b17a
keza26 <kezaglo2021@gmail.com>
2026-05-18 23:11:08 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Student{
+    String Name;
+    int Age;
+    double marks;
+
+}
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
